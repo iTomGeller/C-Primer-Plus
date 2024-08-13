d9f055121459bf8737e2bf6238f21c1e7725a4fe
iTomGeller <303327065@qq.com>
2024-08-13 19:53:05 +0800

    Revert "Chapter 9"
    This reverts commit 0bab7dd688c8de40f9c39398358778e097d8dc8b.

--- Chapter 9/golf/golf.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-const int Len = 40;//默认为static const 但是对应cpp文件依然可以用
-struct golf {
-	char fullname[Len];
-	int handicap;
-};
-void setgolf(golf & g, const char * name, int hc);
-void handicap(golf & g, int hc);
-void showgolf(const golf & g);
